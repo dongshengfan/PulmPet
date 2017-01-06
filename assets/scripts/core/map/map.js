b94dcea1da93699225f9962878be80f872406346
Pavel <shabanov.pavel.andreyevich@gmail.com>
2017-01-07 03:56:57 +0700

    +некоторое пополение функционала у класса карта. Часть 1.

--- assets/scripts/core/map/map.js
+++ assets/scripts/core/map/map.js
@@ -1,19 +1,103 @@
+/**   
+ * Объект игровой карта
+ * @type {Map} игровая карта
+ * @memberOf Maps
+ */
+let _instance=null; 
 
-let instance = null;
 
-export class Maps{  
+/**
+ * Обертка для игровой карты ввиде одиночки 
+ * 
+ * @export
+ * @class Maps
+ */
+export class Maps{ 
+    /**
+     * Creates an instance of Maps.
+     * 
+     * @memberOf Maps
+     */
     constructor() {
-        if(!instance){
-              instance = new Map();
+        if(!_instance){
+            _instance = new Map();
         }
-        return instance;
+        return _instance;
       }
 }
 
-class Map{
-    constructor() {       
+
+/**
+ * Класс игровой карты
+ * 
+ * @class Map
+ */
+class Map{ 
+     
+    /**
+     * Текущая карта
+     * 
+     * @type {cc.TiledMap} Текущая карта
+     * @memberOf Map
+     */
+    _world;
+
+    /**
+     * Размер карты в пикселях
+     * 
+     * @type {cc.Vec2} вектор
+     * @memberOf Map
+     */
+    _sizeMapPixel;
+
+    /**
+     * Размер карты в тайлах
+     * 
+     * @type {cc.Vec2} вектор
+     * @memberOf Map
+     */
+    _sizeMapTield;
+
+    /**
+     * Размер тайла у карты
+     * 
+     * @type {cc.Vec2} вектор
+     * @memberOf Map
+     */
+    _sizeTield;
+
+    /**
+     * Инициализация поступившей карты
+     * 
+     * @memberOf Map
+     */
+    _initializationWorld(){
+        
     }
 
+    /**
+     * Устанавливает текущую карту
+     * 
+     * @param {cc.TiledMap} tiledMap
+     * 
+     * @memberOf Map
+     */
+    setMap(tiledMap){
+        this._world=tiledMap;
+        this._initializationWorld();
+    }
+
+    /**
+     * Возвращает текущую карту
+     * 
+     * @returns {cc.TiledMap} текущая карта
+     *
+     * @memberOf Map
+     */
+    getMap(){
+        return this._world;
+    }
+    
 }
 
 
